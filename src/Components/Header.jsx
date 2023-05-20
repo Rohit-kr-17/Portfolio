61c44c59787e8b5b9cb4c9395f5ea6c49ff2807c
Rohit-kr-17 <rohit.2001.chaurasia@gmail.com>
2023-05-20 22:39:12 +0530

    Created Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,24 +1,31 @@
 import { HashLink as Link } from "react-router-hash-link";
 export default function Header() {
 	return (
-		<>
-			<div className=" top-0 fixed  justify-center  w-full  bg-gray-600  py-[10px] text-white shadow-md shadow-gray-500">
-				<span className="px-2 hover:text-red-700 transiton duration-150 ">
-					<Link to="#home">Home</Link>
-				</span>
-				<span className="px-2 hover:text-red-700 transiton duration-150 ">
-					<Link to="#about">About</Link>
-				</span>
-				<span className="px-2 hover:text-red-700 transiton duration-150 ">
-					<Link to="#skill">Skills</Link>
-				</span>
-				<span className="px-2 hover:text-red-700 transiton duration-150 ">
-					<Link to="#projects">Projects</Link>
-				</span>
-				<span className="px-2 hover:text-red-700 transiton duration-150 ">
-					<Link to="#contacts">Contacts</Link>
-				</span>
+		<div className=" top-0 fixed flex flex-row justify-between items-center w-full  bg-gray-300  py-[10px] text-black shadow-md shadow-gray-500">
+			<div class="ml-8">
+				<Link
+					style={{ fontFamily: "kaushan script" }}
+					className="text-lg"
+					to="#home"
+				>
+					Rohit
+				</Link>
 			</div>
-		</>
+			<div className="flex md:mr-[40%]">
+				<div className="py-1 px-3 hover:bg-slate-400 hover:rounded-full cursor-pointer "></div>
+				<div className="py-1 px-3 hover:bg-slate-400 hover:rounded-full cursor-pointer">
+					<Link to="#about">About</Link>
+				</div>
+				<div className="py-1 px-3 hover:bg-slate-400 hover:rounded-full cursor-pointer ">
+					<Link to="#skill">Skills</Link>
+				</div>
+				<div className="py-1 px-3 hover:bg-slate-400 hover:rounded-full cursor-pointer">
+					<Link to="#projects">Projects</Link>
+				</div>
+				<div className="py-1 px-3 hover:bg-slate-400 hover:rounded-full cursor-pointer ">
+					<Link to="#contacts">Contacts</Link>
+				</div>
+			</div>
+		</div>
 	);
 }
